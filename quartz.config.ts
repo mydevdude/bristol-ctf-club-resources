798513647e61726e1f50c1ac755b7f420758da17
Corey Hayden <cghayden@gmail.com>
2025-10-21 09:41:35 -0400

    Quartz sync: Oct 21, 2025, 9:41 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -89,7 +89,9 @@ const config: QuartzConfig = {
       Plugin.Favicon(),
       Plugin.NotFoundPage(),
       // Comment out CustomOgImages to speed up build time
-      Plugin.CustomOgImages(),
+      Plugin.CustomOgImages({
+        colorScheme: "lightMode",
+      }),
     ],
   },
 }
