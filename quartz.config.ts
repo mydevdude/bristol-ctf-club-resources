24506901f8312c54af249491a533602f432737c7
corey_hayden <cghayden@gmail.com>
2026-02-17 22:04:42 -0500

    changed baseUrl to bristolctf.club

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "bristolctf.com",
+    baseUrl: "bristolctf.club",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
